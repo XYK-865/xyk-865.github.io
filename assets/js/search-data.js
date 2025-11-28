ecc1a0b787664f089a37c774f6bb6e57a04560e8
XYK-865 <Spike_Hsu@outlook.com>
2025-11-28 17:34:45 +0000

    Deploying to gh-pages from @ XYK-865/xyk-865.github.io@70c19b784f675f4ccc0378a222c51a6607e8a983 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-spike-hsu",
+    title: "Spike Hsu",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
